cef7e80fa02539c7743429783533dc5690bfebb8
Diogo O Santos <diogogyn100@gmail.com>
2024-08-29 20:13:38 -0300

    implementando setters e getter

--- src/main/java/org/example/Pessoa.java
+++ src/main/java/org/example/Pessoa.java
@@ -1,13 +1,13 @@
 package org.example;
 
 public class Pessoa {
-    int idade;
-    float peso;
-    float altura;
-    String nome;
-    String cidade;
-    String estado;
-    boolean vivo;
+    private int idade;
+    private float peso;
+    private float altura;
+    private String nome;
+    private String cidade;
+    private String estado;
+    private boolean vivo;
     //construtor padrão
     public Pessoa(){
         this.vivo = true;
@@ -28,5 +28,61 @@ public class Pessoa {
 
     public Pessoa(boolean estaVivo){
         this.vivo = estaVivo;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return this.cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return this.estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean isVivo() {
+        return this.vivo;
+    }
+
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
     }
 }
--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -1,29 +1,45 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
         Pessoa p2 = new Pessoa(100, 50, 1.59f, "Micalteia", "Cairo", "Moafaza",false);
         Pessoa p3 = new Pessoa(false);
         System.out.println("Pessoa 1");
-        System.out.println(p1.nome);
-        System.out.println(p1.peso);
-        System.out.println(p1.altura);
-        System.out.println(p1.cidade);
-        System.out.println(p1.estado);
-        System.out.println(p1.vivo);
+        p1.setNome("Hermanoteu");
+        p1.setPeso(70);
+        p1.setAltura(1.6f);
+        p1.setIdade(45);
+        p1.setCidade("Cairo");
+        p1.setEstado("Moafaza");
+        p1.setVivo(true);
+        System.out.println(p1.getNome());
+        System.out.println(p1.getPeso());
+        System.out.println(p1.getAltura());
+        System.out.println(p1.getCidade());
+        System.out.println(p1.getEstado());
+        System.out.println(p1.isVivo());
 
         System.out.println("Pessoa 2");
-        System.out.println(p2.nome);
-        System.out.println(p2.peso);
-        System.out.println(p2.altura);
-        System.out.println(p2.cidade);
-        System.out.println(p2.estado);
-        System.out.println(p2.vivo);
+        p1.setNome("Micalateia");
+        p1.setPeso(50);
+        p1.setAltura(1.5f);
+        p1.setIdade(40);
+        p1.setCidade("Cairo");
+        p1.setEstado("Moafaza");
+        p1.setVivo(true);
+        System.out.println(p1.getNome());
+        System.out.println(p1.getPeso());
+        System.out.println(p1.getAltura());
+        System.out.println(p1.getCidade());
+        System.out.println(p1.getEstado());
+        System.out.println(p1.isVivo());
+
 
         System.out.println("Pessoa 3");
-        System.out.println(p3.vivo);
-
+        System.out.println(p3.isVivo());
 
 
     }
